a270d5f96a70025b7630b6843d6deba7139203b7
Tessavdg <tessa@grijspaarde.nl>
2022-05-18 16:58:07 +0200

    Nieuwe animaties

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -2,12 +2,12 @@
 // Op die mommenten komt er een class op de body.
 sounds = [
 	0,
-	1,
+	2,
 	9,
 	12,
-	15,
+	15.3,
 	16.5,
-	24,
+	24.3,
 	25.8,
 	31,
 	32,
@@ -15,3 +15,8 @@ sounds = [
 	86,
 	88,
 ];
+
+// document.querySelector('body').addEventListener('animationEnd',function(){
+// 	this.style.animationName='';
+// 	console.log('Animation ended');
+// });
